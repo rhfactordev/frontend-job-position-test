2c35f612c95e77a86bfccc8d726e8326f9fddc59
ewdemy <ewdemy@hotmail.com>
2021-03-06 11:40:02 -0300

    Tela de listar clientes

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-function chamaHome(event){
+function login(event){
     event.preventDefault()
     let user = document.querySelector("#usuario")
     let senha = document.querySelector("#senha")
     if(user.value === "admin" && senha.value === "admin"){
         localStorage.setItem("user", user.value)
-        window.location.href="index.html"
+        window.location.href="home.html"
     } else{
         alert("Usuário e/ou senha incorretos!")
         user.value = ""
@@ -18,4 +18,20 @@ function setNomeUser(){
     let user = localStorage.getItem("user")
     let userSpan = document.querySelector("#user") 
     userSpan.innerHTML = user
+}
+function logout(){
+    localStorage.removeItem("user")
+    window.location.href="login.html"
+}
+
+class Cliente = {
+    constructor(nome, email, dataNasc, cpf, endereco, telefone, sexo){
+        this.nome = nome
+        this.email = email
+        this.dataNasc = dataNasc
+        this.cpf = cpf
+        this.endereco = endereco
+        this.telefone = telefone
+        this.sexo = sexo
+    }
 }
